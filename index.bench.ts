2d8a41906b1fbe8c5fab1fb896dd7c618258f178
Girges Scandar <girges.scandar@getyourguide.com>
2024-06-18 18:10:27 +0200

    benching template engines

---
+++ index.bench.ts
@@ -0,0 +1,106 @@
+import { bench } from "vitest";
+import pug from "pug";
+import Handlebars from "handlebars";
+import Mustache from "mustache";
+import { readFileSync } from "fs";
+
+const iterations = 20000;
+const str = "a".repeat(100_000);
+
+const file = readFileSync("template.html").toString();
+const compiledPug = pug.compileFile("template.pug");
+const compiledHB = Handlebars.compile(file);
+
+bench(
+  "pug",
+  () => {
+    compiledPug({
+      one: str,
+      two: str,
+      three: str,
+      four: str,
+      five: str,
+      six: str,
+      seven: str,
+      eight: str,
+      nine: str,
+      ten: str,
+      eleven: str,
+    });
+  },
+  { iterations }
+);
+
+bench(
+  "handlebars",
+  () => {
+    compiledHB({
+      one: str,
+      two: str,
+      three: str,
+      four: str,
+      five: str,
+      six: str,
+      seven: str,
+      eight: str,
+      nine: str,
+      ten: str,
+      eleven: str,
+    });
+  },
+  { iterations }
+);
+
+bench(
+  "mustache",
+  () => {
+    Mustache.render(file, {
+      one: str,
+      two: str,
+      three: str,
+      four: str,
+      five: str,
+      six: str,
+      seven: str,
+      eight: str,
+      nine: str,
+      ten: str,
+      eleven: str,
+    });
+  },
+  { iterations }
+);
+
+bench(
+  "replace",
+  () => {
+    const html = `
+    <html>
+      <!-- one -->
+      <!-- two -->
+      <!-- three -->
+      <!-- four -->
+      <!-- five -->
+      <!-- six -->
+      <!-- seven -->
+      <!-- eight -->
+      <!-- nine -->
+      <!-- ten -->
+      <!-- eleven -->
+    </html>
+  `;
+    html
+      .replace("<!-- one -->", str)
+      .replace("<!-- two -->", str)
+      .replace("<!-- three -->", str)
+      .replace("<!-- four -->", str)
+      .replace("<!-- five -->", str)
+      .replace("<!-- six -->", str)
+      .replace("<!-- seven -->", str)
+      .replace("<!-- eight -->", str)
+      .replace("<!-- nine -->", str)
+      .replace("<!-- ten -->", str)
+      .replace("<!-- eleven -->", str);
+  },
+  { iterations }
+);
